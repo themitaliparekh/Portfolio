88dbd80be37abcd9e13280cc860d98404643feef
themitaliparekh <mitali00@icloud.com>
2022-07-01 10:59:00 -0400

    Portfolio!

---
+++ script.js
@@ -0,0 +1,20 @@
+  const dropdownOpen = document.querySelector(".dropdown-open");
+
+  Array.from(document.querySelectorAll(".nav-item")).forEach((item) => {
+    item.onmouseover = () => {
+      dropdownOpen.style.opacity = 1;
+      dropdownOpen.style.visibility = "visible";
+      dropdownOpen.style.width = getComputedStyle(item.lastElementChild).width;
+      dropdownOpen.style.height = getComputedStyle(item.lastElementChild).height;
+
+      dropdownOpen.style.top = `${item.lastElementChild.offsetTop}px`;
+      dropdownOpen.style.left = `${item.lastElementChild.offsetLeft}px`;
+    };
+
+    item.onmouseout = () => {
+      dropdownOpen.style.opacity = 0;
+      dropdownOpen.style.visibility = "hidden";
+    };
+  });
+
+
